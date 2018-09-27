f89717e8ab009c36c96355b933dd0bde9acd1099
ganature <498857839@qq.com>
2018-09-27 11:17:49 +0800

    func add readTxt

--- func.py
+++ func.py
@@ -1,3 +1,11 @@
 #coding=utf-8
 
 import os
+
+
+def readTxt(file_name):
+    data=[]
+    with open(file_name,'rb')as file_content:
+        for f in file_content.readlines():
+            data.append(f)
+    return ''.join(data)
